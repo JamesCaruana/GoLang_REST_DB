1a4e1780dac9f9afc26541157817f1ea41ffc997
James Caruana <caruana.james22@gmail.com>
2021-07-21 14:24:45 +0200

    Created the get all function
    Added the get all function in main.go

--- main.go
+++ main.go
@@ -1,32 +1,64 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"go_rad/db"
+	"go_rad/models"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var collection = db.ConnectDb()
 
 func main() {
-	//Init Router
+	// Subrouter
 	r := mux.NewRouter()
 
+	// Create the handles for each
 	r.HandleFunc("/api/users", getUsers).Methods(http.MethodGet)
-	r.HandleFunc("/api/users/{id}", getUserById).Methods(http.MethodGet)
-	r.HandleFunc("/api/user", createUser).Methods(http.MethodPut)
-	r.HandleFunc("/api/users/{id}", updateUser).Methods(http.MethodPut)
-	r.HandleFunc("/api/users/{id}", deleteUser).Methods(http.MethodDelete)
-
+	//r.HandleFunc("/api/users/{id}", getUserById).Methods(http.MethodGet)
+	//r.HandleFunc("/api/user", createUser).Methods(http.MethodPut)
+	//r.HandleFunc("/api/users/{id}", updateUser).Methods(http.MethodPut)
+	//r.HandleFunc("/api/users/{id}", deleteUser).Methods(http.MethodDelete)
+	log.Fatal(http.ListenAndServe("localhost:27017", r))
 }
 
-func getUsers()
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
-func getUserById()
+	// Array of struct users
+	var users []models.User
 
-func createUser()
+	cur, err := collection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		db.GetError(err, w)
+	}
 
-func updateUser()
+	defer cur.Close(context.TODO())
 
-func deleteUser()
+	for cur.Next(context.TODO()) {
+		var user models.User
+		err := cur.Decode(&user)
+		if err != nil {
+			log.Fatal(err)
+		}
+		users = append(users, user)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+//func getUserById()
+
+//func createUser()
+
+//func updateUser()
+
+//func deleteUser()
